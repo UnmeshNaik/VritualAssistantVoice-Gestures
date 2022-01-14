6e9f15fb192b9416f1baa2fc471c8c0101567473
UnmeshNaik <83971806+UnmeshNaik@users.noreply.github.com>
2022-01-14 17:06:12 +0530

    Add files via upload

---
+++ Scrolling.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+import HandTrackingModule as Htm
+import time
+import autopy
+import pyautogui
+
+wCam, hCam = 640, 480
+# Frame reduction :- Due to which we are easily able to detect our hand in certain frame
+# and the pointer moves up to corners easily.
+frameR = 100
+# smoothening variable is used to make mouse move freely.
+smoothening = 7
+
+pTime = 0
+plocX, plocY = 0, 0
+clocX, clocY = 0, 0
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+# This handDetector function is imported from the HandTrackingModule.
+# And detector is the general variable which shows the handDetector function.
+detector = Htm.handDetector(maxHands=1)
+wScr, hScr = autopy.screen.size()
+print(wScr, hScr)
+
+count = 1
+
+while True:
+
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+
+    if len(lmList) != 0:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+
+        # print(x1, y1, x2, y2)
+
+        fingers = detector.fingersUp()
+        # print(fingers)
+        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
+                      (255, 255, 0), 2)
+
+        if fingers[1] == 1 and fingers[2] == 0:
+
+            x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
+            y3 = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
+
+            clocX = plocX + (x3 - plocX) / smoothening
+            clocY = plocY + (y3 - plocY) / smoothening
+
+            autopy.mouse.move(wScr - clocX, clocY)
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            plocX, plocY = clocX, clocY
+
+        elif fingers[1] == 1 and fingers[2] == 1:
+            if count==1:
+                prevloc = currloc = y1
+                count = 0
+                diff = 0
+            else:
+                diff = y1 - prevloc
+                prevloc = y1
+            length, img, lineInfo = detector.findDistance(8, 12, img)
+            print(length,y1)
+            sensitivity = 3
+            if length < 40:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                #autopy.mouse.click()
+                #autopy.mouse.toggle(autopy.mouse.Button.MIDDLE, True)
+                pyautogui.scroll(-diff*sensitivity)
+            else:
+                pyautogui.scroll(diff*sensitivity)
+        else:
+            count = 1
+
+    cTime = time.time()
+    fps = 1 / (cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
